74f96d5f5823fd664ec2f381a315ef2a1be87ed9
Kieran Murray <kohyr@rocketmail.com>
2020-11-07 13:09:43 +0000

    Add function for processing Select2 Dynamic Data.

--- products/forms.py
+++ products/forms.py
@@ -15,7 +15,7 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = 'artist',
+        fields = 'artist','label',
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -23,16 +23,15 @@ class ProductForm(forms.ModelForm):
         artist = Artist.objects.all()
         friendly_names = [(a.id, a.get_friendly_name()) for a in artist]
         self.fields['artist'].choices = friendly_names
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'single-dynamic'
+
 
     #     genre = Genre.objects.all()
     #     friendly_names = [(g.id, g.get_friendly_name()) for g in genre]
     #     self.fields['genre'].choices = friendly_names
 
-    #     label = Label.objects.all()
-    #     friendly_names = [(l.id, l.get_friendly_name()) for l in label]
-    #     self.fields['label'].choices = friendly_names
+        label = Label.objects.all()
+        friendly_names = [(l.id, l.get_friendly_name()) for l in label]
+        self.fields['label'].choices = friendly_names
 
     #     colour = Colour.objects.all()
     #     friendly_names = [(c.id, c.get_friendly_name()) for c in colour]
@@ -45,3 +44,5 @@ class ProductForm(forms.ModelForm):
     #     tag = Tag.objects.all()
     #     friendly_names = [(t.id, t.get_friendly_name()) for t in tag]
     #     self.fields['tags'].choices = friendly_names
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'single-dynamic'
--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .forms import ProductForm
-from .models import Product
+from .models import Product, Artist, Label
 
 # Create your views here.
 
@@ -13,15 +13,19 @@ def add_product(request):
 
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
+        add_dynamically_created_data_to_the_database()
         if form.is_valid():
+
+            print("Form is valid")
+            print(form.cleaned_data['artist'])
             # Generate SKU
-            sku = str(form.cleaned_data['name'])[0:3]+"/"+str(form.cleaned_data['artist'])[0:3]+"/"+str(form.cleaned_data['label'])[0:3]+"/"+str(form.cleaned_data['colour'])[0:3]+"/"+str(form.cleaned_data['format'])[0:3]
-            sku = sku.upper()
-            
-            product = form.save()
-            product.sku = sku
-            product.save()
-            return redirect(reverse('add_product'))
+            # sku = str(form.cleaned_data['name'])[0:3]+"/"+str(form.cleaned_data['artist'])[0:3]+"/"+str(form.cleaned_data['label'])[0:3]+"/"+str(form.cleaned_data['colour'])[0:3]+"/"+str(form.cleaned_data['format'])[0:3]
+            # sku = sku.upper()
+
+            # product = form.save()
+            # product.sku = sku
+            # product.save()
+            return redirect(reverse('home'))
     else:
         form = ProductForm()
 
@@ -31,3 +35,28 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def add_dynamically_created_data_to_the_database():
+    # Check if there is an Artist not in the database
+    artist_submitted_in_the_form = form.data['artist']
+    if(not artist_submitted_in_the_form.isnumeric()):
+        # Create a new artist and insert it into the database
+        new_artist_name = artist_submitted_in_the_form.lower()
+        new_artist_name = new_artist_name.replace(" ", "_")
+        new_artist_friendly_name = artist_submitted_in_the_form
+        new_artist = Artist()
+        new_artist.name = new_artist_name
+        new_artist.friendly_name = new_artist_friendly_name
+        new_artist.save()
+
+    label_submitted_in_the_form = form.data['label']
+    if(not label_submitted_in_the_form.isnumeric()):
+        # Create a new label and insert it into the database
+        new_label_name = label_submitted_in_the_form.lower()
+        new_label_name = new_label_name.replace(" ", "_")
+        new_label_friendly_name = label_submitted_in_the_form
+        new_label = Label()
+        new_label.name = new_label_name
+        new_label.friendly_name = new_label_friendly_name
+        new_label.save()
