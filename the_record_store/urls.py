c575edd7305a637084712a72d31f27e2f7c1b396
Kieran <kohyr@rocketmail.com>
2020-11-16 12:01:37 +0000

    Add Add To Cart Functionality.

--- the_record_store/urls.py
+++ the_record_store/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-    path('products/', include('cart.urls')),
+    path('cart/', include('cart.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
