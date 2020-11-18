d00ab00ad3e2b818bde9146137c111f90041b00a
Kieran <kohyr@rocketmail.com>
2020-11-18 11:11:17 +0000

    Add Wishlist Remove All + Add All To Cart.

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.view_wishlist, name='view_wishlist'),
     path('add/<item_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('add_all/', views.add_wishlist_to_cart, name='add_wishlist_to_cart'),
     path('remove/<item_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path('remove_all/', views.remove_all_from_wishlist, name='remove_all_from_wishlist'),
+    # path('share/', views.share_wishlist, name='share_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -4,26 +4,26 @@ from django.contrib import messages
 from products.models import Product
 # Create your views here.
 
+
 def view_wishlist(request):
     """ A view to show contents of the wishlist """
     return render(request, 'wishlist/view_wishlist.html')
+
 
 def add_to_wishlist(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     wishlist = request.session.get('wishlist', {})
 
-    
-    
     if item_id in list(wishlist.keys()):
         messages.success(request, f'{product.name} already in your wishlist')
     else:
         wishlist[item_id] = 1
         messages.success(request, f'Added {product.name} to your wishlist')
 
-
     request.session['wishlist'] = wishlist
     return redirect(redirect_url)
+
 
 def remove_from_wishlist(request, item_id):
 
@@ -35,3 +35,29 @@ def remove_from_wishlist(request, item_id):
     return redirect(redirect_url)
 
 
+def remove_all_from_wishlist(request):
+
+    wishlist = request.session.get('wishlist', {})
+    wishlist.clear()
+    request.session['wishlist'] = wishlist
+    redirect_url = request.POST.get('redirect_url')
+    return redirect(redirect_url)
+
+
+def add_wishlist_to_cart(request):
+
+    wishlist = request.session.get('wishlist', {})
+    print(wishlist)
+    cart = request.session.get('cart', {})
+
+    for wishlist_item in wishlist:
+        cart[wishlist_item] = 1
+        print(wishlist_item)
+    
+    request.session['cart'] = cart
+    redirect_url = request.POST.get('redirect_url')
+    return redirect(redirect_url)
+
+# def share_wishlist(request):
+#     redirect_url = request.POST.get('redirect_url')
+#     return redirect(redirect_url)
