a0a48e708c13ccf2a1f401f21ddc9d0809ec4ef6
Kieran <kohyr@rocketmail.com>
2020-11-20 18:32:59 +0000

    Add Order model.

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+import uuid
+from profiles.models import UserProfile
+from django_countries.fields import CountryField
+from phone_field import PhoneField
 
 # Create your models here.
+
+
+class Order(models.Model):
+
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    address_line_1 = models.CharField(max_length=80, null=False, blank=False)
+    address_line_2 = models.CharField(max_length=80, null=False, blank=False)
+    town_or_city = models.CharField(max_length=80, null=False, blank=False)
+    county_or_province = models.CharField(max_length=80, null=False,
+                                          blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    post_code_or_zip = models.CharField(max_length=80, null=False, blank=False)
+    phone_number = PhoneField(blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
+                                        null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+
+    def _generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __(self):
+        return self.order_number
--- profiles/models.py
+++ profiles/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from phone_field import PhoneField
+from django_countries.fields import CountryField
 # Create your models here.
 
+
 class Address(models.Model):
-    
-    user = models.ForeignKey(User,null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=254)
     last_name = models.CharField(max_length=254)
     address_line_1 = models.CharField(max_length=254)
     address_line_2 = models.CharField(max_length=254)
     town_or_city = models.CharField(max_length=254)
     county_province = models.CharField(max_length=254)
-    country = models.CharField(max_length=254)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     post_code_zip_code = models.CharField(max_length=254)
     phone_number = PhoneField(blank=True)
     primary_address = models.BooleanField(default=False)
@@ -21,11 +22,11 @@ class Address(models.Model):
         return self.address_line_1
 
 
-
 class UserProfile(models.Model):
-    
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_address =  models.ForeignKey(Address, null=True, on_delete=models.CASCADE)
+    default_address = models.ForeignKey(
+        Address, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
