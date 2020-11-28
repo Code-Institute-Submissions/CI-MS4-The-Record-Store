cca4ed418a9939428e5009415958d68fbe01ccac
Kieran <kohyr@rocketmail.com>
2020-11-28 15:56:21 +0000

    Update allowed hosts.

--- the_record_store/settings.py
+++ the_record_store/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ci-ms4.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['the-record-store.herokuapp.com', '127.0.0.1']
 SITE_ID = 1
 
 # Application definition
