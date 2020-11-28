7061740da046a6e0f3ea16038632c3315dd2bdbe
Kieran <kohyr@rocketmail.com>
2020-11-28 16:29:50 +0000

    Add base webhook handling.

--- the_record_store/settings.py
+++ the_record_store/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
+import django_heroku
 import dj_database_url
 import os
 from dotenv import load_dotenv
 load_dotenv()
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
