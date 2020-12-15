adf03ea987c5ab1bd251bbecc20911937e989673
Kieran <kohyr@rocketmail.com>
2020-12-15 18:47:35 +0000

    Fix order creation in Webhook.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -49,9 +49,11 @@ class StripeWH_Handler:
         pid = intent.id
         print(f'pid = {pid}')
         billing_details = intent.charges.data[0].billing_details
+        first_name, last_name = billing_details.name.split(" ", 1)
         cart = intent.metadata.cart
         save_info = intent.metadata.save_info
         grand_total = round(intent.charges.data[0].amount / 100, 2)
+        profile = None
 
         print('Checking if order exists')
         order_exists = False
@@ -68,7 +70,40 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
 
-        return HttpResponse(content=f'Webhook received: {event["type"]}', status=200)
+        if order_exists:
+            self._send_confirmation_email(order)
+            return HttpResponse(content=(f'Webhook received: {event["type"]} | SUCCESS: ' 'Verified order already in database'), status=200)
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    first_name=first_name,
+                    last_name=last_name,
+                    user_profile=profile,
+                    email=billing_details.email,
+                    phone_number=billing_details.phone,
+                    country=billing_details.address.country,
+                    postcode=billing_details.address.postal_code,
+                    town_or_city=billing_details.address.city,
+                    street_address1=billing_details.address.line1,
+                    street_address2=billing_details.address.line2,
+                    county=billing_details.address.state,
+                    stripe_pid=pid,
+                )
+                for item_id, item_data in json.loads(cart).items():
+                    product = Product.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data,)
+                    order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(content=f'Webhook received: {event["type"]} | ERROR: {e}', status=500)
+
+        self._send_confirmation_email(order)
+        return HttpResponse(content=(f'Webhook received: {event["type"]} | SUCCESS: ''Created order in webhook'), status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         """
