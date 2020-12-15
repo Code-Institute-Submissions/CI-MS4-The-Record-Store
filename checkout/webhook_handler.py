a95296c33a8758d5307dc2d150d541fd65c449b2
Kieran <kohyr@rocketmail.com>
2020-12-15 14:03:20 +0000

    Add Log To Send Mail to test mail sending.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -27,7 +27,7 @@ class StripeWH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-
+        print(f"sending email to {cust_email}")
         send_mail(
             subject,
             body,
